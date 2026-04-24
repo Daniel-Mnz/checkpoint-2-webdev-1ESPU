47ed77b47879b55b59d03ffd3a3e167f019d9cec
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-23 21:39:36 -0300

    questão 1 - condicional IF

---
+++ participantes.md
@@ -0,0 +1,8 @@
+Daniel Muniz Campos de Lima
+RM: 573683
+
+Murilo Ribeiro Vasconcellos
+RM: 569614
+
+Miguel Rosendo Miranda Ramos
+RM: 571752
---
+++ script.js
@@ -0,0 +1,7 @@
+//Questão 1 – Condicional If
+const tarefaConcluida = "2";
+if (tarefaConcluida===2){
+    console.log("A tarefa está concluída!");
+} else{
+    console.log("A tarefa não foi concluida");
+}
