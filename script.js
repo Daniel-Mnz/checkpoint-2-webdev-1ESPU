e94f7826a84102a0cb7b18af13a0a5cbe9eb73cf
Daniel-Mnz <czpcamposlima@gmail.com>
2026-04-23 21:56:38 -0300

    Questão 3 - Switch Case

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ if (prioridade === 1){
 } else{
     console.log("Esse valor não se encaixa como uma prioridade");
 }
+
+//Questão 3 – Switch Case
+let diaSemana = new Date().getDay();
+
+switch(diaSemana){
+    case 0:
+        console.log("Domingo");
+        break;
+    case 1:
+        console.log("Segunda-feira");
+        break;
+    case 2:
+        console.log("Terça-feira");
+        break;
+    case 3:
+        console.log("Quarta-feira");
+        break;
+    case 4:
+        console.log("Quinta-feira");
+        break;
+    case 5:
+        console.log("Sexta-feira");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+    
+}
+
