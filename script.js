b5e7adf0e552933d6001828271a36cc4c21dc2df
Daniel-Mnz <czpcamposlima@gmail.com>
2026-04-23 21:44:58 -0300

    Questão 2 - Condicional If/Else If/Else

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ if (tarefaConcluida===2){
 } else{
     console.log("A tarefa não foi concluida");
 }
+
+//Questão 2 – Condicional If/Else If/Else
+ 
+let prioridade = 2;
+if (prioridade === 1){
+    console.log("Prioridade Baixa");
+} else if(prioridade === 2){
+    console.log("Prioridade Média");
+} else if(prioridade === 3){
+    console.log("Prioridade Alta");
+} else{
+    console.log("Esse valor não se encaixa como uma prioridade");
+}
